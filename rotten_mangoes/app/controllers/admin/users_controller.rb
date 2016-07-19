c9d1edc6773dd65345ebfc7318f470b7f64714c4
vagrant <vagrant@lighthouse-labs>
2016-07-19 20:08:57 +0000

    adding admin user list

--- rotten_mangoes/app/controllers/admin/users_controller.rb
+++ rotten_mangoes/app/controllers/admin/users_controller.rb
@@ -3,9 +3,31 @@ require 'pry'
 class Admin::UsersController < ApplicationController
   before_filter :admin_only
 
-
   def index
-    @users = User.all
+    @users = User.all.order(params[:sort])
   end
 
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    @user.save
+    redirect_to admin_users_path
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  protected
+
+  def user_params
+    params.require(:user).permit(:email, :firstname, :lastname, :password, :password_confirmation, :admin)
+  end
 end
--- rotten_mangoes/config/routes.rb
+++ rotten_mangoes/config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
   resources :sessions, only: [:new, :create, :destroy]
 
   namespace :admin do
-    resources :users
+    resources :users, only: [:index, :show, :edit, :create]
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
